f0d0dfdaa45d290b65ed6350145885f8894806b1
Mathew Sojan <mathewsojan@Mathews-MacBook-Pro.local>
2018-11-12 17:56:16 -0800

    added reducers

--- LinkedIn-Kafka-Backend/mongoose.js
+++ LinkedIn-Kafka-Backend/mongoose.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 //"mongodb://localhost:27017/HomeAway",
 // "mongodb://sojanmathew:sojanm28@ds133920.mlab.com:33920/homeaway",
 mongoose.connect(
-  "mongodb://linkedin:linkedteam1@ds159263.mlab.com:59263/linkedin",
+  "mongodb://linkedin:linkedinteam1@ds159263.mlab.com:59263/linkedin",
 
   options
 );
