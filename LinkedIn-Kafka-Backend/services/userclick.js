67166b93ff94e56fee3c048338db79cbd3baddc1
Amruta Saraf <amrutasaraf@Amrutas-MacBook-Pro.local>
2018-11-24 16:09:47 -0800

    change to userclick

--- LinkedIn-Kafka-Backend/services/userclick.js
+++ LinkedIn-Kafka-Backend/services/userclick.js
@@ -9,19 +9,26 @@ function handle_request(msg, callback) {
    jobData: msg.body.jobData
  });
 
- var promise = myobj.save();
- promise
-   .then(function() {
-     res.value = msg;
-     res.code = 200;
-     callback(null, res);
-   })
+//  var promise = myobj.save();
+//  promise
+//    .then(function() {
+//      res.value = msg;
+//      res.code = 200;
+//      callback(null, res);
+//    })
 
-   .catch(function(err) {
-     console.log("error:", err.message);
+//    .catch(function(err) {
+//      console.log("error:", err.message);
 
-     res.code = "400";
-     callback(null, res);
-   });
+//      res.code = "400";
+//      callback(null, res);
+//    });
+myobj.save().then((doc)=>{
+  console.log('Result fromkakfka', doc);
+  callback(null, doc);
+}, (err) =>{
+  callback(err, null);
+
+});
 }
 exports.handle_request = handle_request;
